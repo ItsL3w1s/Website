a5a2cc713ab2a093b9e800aa33203960186c93ef
Dominic Maas <dominic.maas@live.com>
2020-06-03 09:47:57 +1200

    Updates

---
+++ precache-manifest.9ee64edd5859d4b663c5a45dfa1a8b24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b398da91f777654f2667ac0f549ee40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb0b15162f54ecc1cd8d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a9ba37ca018b4195f08",
+    "url": "/static/js/2.cf95bb35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf95bb35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0b15162f54ecc1cd8d",
+    "url": "/static/js/main.f25a107f.chunk.js"
+  },
+  {
+    "revision": "4d3686bc947f6fd00750",
+    "url": "/static/js/runtime-main.5c4f518f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
